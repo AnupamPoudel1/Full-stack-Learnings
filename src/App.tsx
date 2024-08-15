432df830b36143c05f19cbb367614e1ab5d36eb6
Anupam <anupampoudel78@gmail.com>
2024-08-15 20:54:02 +0545

    simple class component example in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header"
+import Header from "./components/classHeader";
 
 const App = (): JSX.Element => {
   return (
---
+++ src/components/classHeader.tsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="bg-gray-900 text-white text-xl h-max px-8 py-4 w-full flec justify-center items-center">
+                Header
+            </div>
+        )
+    }
+}
+
+export default Header;
