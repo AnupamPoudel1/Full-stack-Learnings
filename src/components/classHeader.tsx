db980de358a9d72013ade50f9d41996488f2ddd1
Anupam <anupampoudel78@gmail.com>
2024-08-15 21:47:51 +0545

    different types of styling techiniques in react

--- src/components/classHeader.tsx
+++ src/components/classHeader.tsx
@@ -2,9 +2,24 @@ import { Component } from "react";
 
 class Header extends Component {
     render() {
+
+        const style = {
+            header: {
+                background: '#ffffff',
+                color: '#000000',
+                textAlign: 'center',
+            },
+            logo: {
+                color: '#000000',
+                background: '#ffffff'
+            }
+        }
+
         return (
-            <div className="bg-gray-900 text-white text-xl h-max px-8 py-4 w-full flec justify-center items-center">
-                Header
+            <div className="flex flex-col h-max w-full justify-center items-center bg-black px-8 py-4">
+                <div style={style.logo} className="px-4 py-2 header">
+                    Logo
+                </div>
             </div>
         )
     }
